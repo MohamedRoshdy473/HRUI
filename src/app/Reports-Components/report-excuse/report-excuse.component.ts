3ce9b93de1274a4348b92ca26920f062e1be042c
MohamedRoshdy473 <60385152+MohamedRoshdy473@users.noreply.github.com>
2021-03-16 16:02:42 +0200

    edit in filter

--- src/app/Reports-Components/report-excuse/report-excuse.component.ts
+++ src/app/Reports-Components/report-excuse/report-excuse.component.ts
@@ -15,34 +15,35 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ReportExcuseComponent implements OnInit {
   Allexcuses: any[];
-  FilteredExcuses:any;
-  NewExcuse:any;
+  FilteredExcuses: any;
+  NewExcuse: any;
   AllEmployeesByProfession: any;
   AllProfessions: any;
-  Idtranslate:any
+  Idtranslate: any
   emptranslate: any;
-  Professiontranslate:any;
-  Datetranslate:any;
-  Timetetranslate:any;
-  Hourstranslate:any;
-  Statustranslate:any;
+  Professiontranslate: any;
+  Datetranslate: any;
+  Timetetranslate: any;
+  Hourstranslate: any;
+  Statustranslate: any;
   Excusestranslate: any;
   ALMostakbaltranslate: any;
-  constructor(private translate: TranslateService,private ExcuseService: ExcuseService,private professionService:ProfessionService, private datePipe: DatePipe,private EmpService:EmployeeService) { }
+  lang: string;
+  constructor(private translate: TranslateService, private ExcuseService: ExcuseService, private professionService: ProfessionService, private datePipe: DatePipe, private EmpService: EmployeeService) { }
 
   ngOnInit(): void {
-    this.NewExcuse = {employeeId:0, approved: "pending", employeeName: '', id: 0, profession: '', startdate: new Date(Date.now()),endDate:new Date(), comment: '', hours: 0, time: { hours: 0, minutes: 0 } };
+    this.NewExcuse = { employeeId: 0, approved: "pending", employeeName: '', id: 0, profession: '', startdate: new Date(), endDate: new Date(), comment: '', hours: 0, time: { hours: 0, minutes: 0 } };
     this.Allexcuses = []
     this.ExcuseService.AllExcuses().subscribe(
       data => {
-        this.Allexcuses = data; 
-        this.FilteredExcuses=data
+        this.Allexcuses = data;
+        this.FilteredExcuses = data
         console.log("data", data)
       });
-      this.professionService.getAllProfession().subscribe(
-        data=>{this.AllProfessions=data,console.log("AllProfessions",data)},
-        error => console.log(error)
-      );
+    this.professionService.getAllProfession().subscribe(
+      data => { this.AllProfessions = data, console.log("AllProfessions", data) },
+      error => console.log(error)
+    );
   }
   onChange(ProfessionId) {
     this.EmpService.GetAllEmployeesByProfession(ProfessionId).subscribe(
@@ -55,24 +56,21 @@ export class ReportExcuseComponent implements OnInit {
   }
   typeName: any;
   getReport() {
-    this.translate.get(['HR.AL-Mostakbal','HR.Excuses','HR.Id','HR.Employee Name','HR.Profession Name','HR.Date','HR.Time','HR.Hours','HR.Status']).subscribe((data:any)=> {
-      console.log("Translated data",data);
-      this.ALMostakbaltranslate=data['HR.AL-Mostakbal']
-      this.Excusestranslate=data['HR.Excuses']
-      this.Idtranslate=data['HR.Id']
-      this.emptranslate=data['HR.Employee Name']
-      this.Professiontranslate=data['HR.Profession Name']
-      this.Datetranslate=data['HR.Date']
-      this.Timetetranslate=data['HR.Time']
-      this.Hourstranslate=data['HR.Hours']
-      this.Statustranslate=data['HR.Status']
-
-      console.log("emptranslate data",this.emptranslate);
-
-     });
-
+    this.translate.get(['HR.AL-Mostakbal', 'HR.Excuses', 'HR.Id', 'HR.Employee Name', 'HR.Profession Name', 'HR.Date', 'HR.Time', 'HR.Hours', 'HR.Status']).subscribe((data: any) => {
+      console.log("Translated data", data);
+      this.ALMostakbaltranslate = data['HR.AL-Mostakbal']
+      this.Excusestranslate = data['HR.Excuses']
+      this.Idtranslate = data['HR.Id']
+      this.emptranslate = data['HR.Employee Name']
+      this.Professiontranslate = data['HR.Profession Name']
+      this.Datetranslate = data['HR.Date']
+      this.Timetetranslate = data['HR.Time']
+      this.Hourstranslate = data['HR.Hours']
+      this.Statustranslate = data['HR.Status']
+    });
     const totalPagesExp = "{total_pages_count_string}";
     var doc = new jsPDF('l');
+    this.lang = localStorage.getItem("lang");
     doc.addFont("../assets/ARIALUNI.TTF", "ARIALUNI", "normal");
     doc.setFont('ARIALUNI');
 
@@ -80,30 +78,30 @@ export class ReportExcuseComponent implements OnInit {
     img.src = 'assets/images/logo.jpeg'
     doc.addImage(img, 'png', 10, 10, 23, 23);
     doc.text(this.ALMostakbaltranslate, 50, 25, { "align": "left" });
-    doc.text( this.Excusestranslate, 10, 45, { "align": "left" });
+    doc.text(this.Excusestranslate, 10, 45, { "align": "left" });
 
     var col = [this.Idtranslate, this.emptranslate, this.Professiontranslate, this.Datetranslate, this.Timetetranslate, this.Hourstranslate, this.Statustranslate];
     var rows = [];
     var row = [];
     //var type = this.Allexcuses.reduce((typeName, el) =>  el.employeeName,'Ekram');
-    console.log("this.NewExcuse.employeeId",this.NewExcuse.employeeId)
-    var type = this.Allexcuses.filter(ex => ex.professionID == this.NewExcuse.professionID || ex.employeeId == this.NewExcuse.employeeId
-      ||(ex.date>this.NewExcuse.startdate && ex.date<this.NewExcuse.endDate));
+    console.log("this.NewExcuse.employeeId", this.NewExcuse.employeeId)
+    var type = this.Allexcuses.filter(ex => ex.professionID == this.NewExcuse.professionID
+      || (ex.professionID == this.NewExcuse.professionID && ex.employeeId == this.NewExcuse.employeeId)
+      || (ex.date > this.NewExcuse.startdate && ex.date < this.NewExcuse.endDate));
     console.log("type", type);
-    if(type.length==0)
-    {
-      row = [type,{ startY: 50, styles: { font: 'ARIALUNI' , colSpan:8} }]
+    if (type.length == 0) {
+      row = [type, { startY: 50, styles: { font: 'ARIALUNI', colSpan: 8 } }]
       this.Allexcuses.forEach(element => {
         var ExcuseDate = this.datePipe.transform(element.date, "dd/MM/yyyy");
-        row = [element.id, element.employeeName, element.professionName, ExcuseDate,element.time,element.hours,element.approved];
+        row = [element.id, element.employeeName, element.professionName, ExcuseDate, element.time, element.hours, element.approved];
         rows.push(row);
       });
     }
-    if(type.length!=0){
-      row = [type,{ startY: 50, styles: { font: 'ARIALUNI' , colSpan:8} }]
+    if (type.length != 0) {
+      row = [type, { startY: 50, styles: { font: 'ARIALUNI', colSpan: 8 } }]
       type.forEach(element => {
         var ExcuseDate = this.datePipe.transform(element.date, "dd/MM/yyyy");
-        row = [element.id, element.employeeName, element.professionName, ExcuseDate,element.time,element.hours,element.approved];
+        row = [element.id, element.employeeName, element.professionName, ExcuseDate, element.time, element.hours, element.approved];
         rows.push(row);
       });
     }
@@ -114,14 +112,45 @@ export class ReportExcuseComponent implements OnInit {
     //   console.log("row", row)
     //   rows.push(row);
     // }
-   
+
     (doc as any).autoTable(col, rows, { startY: 50, styles: { font: 'ARIALUNI' } });
-    
+
     doc.save('execuse.pdf');
   }
   Filter() {
-    var type2 = this.Allexcuses.filter(ex => ex.professionID == this.NewExcuse.professionID ||(ex.professionID == this.NewExcuse.professionID && ex.employeeId == this.NewExcuse.employeeId) 
-      ||(ex.date>this.NewExcuse.startdate && ex.date<this.NewExcuse.endDate));
-      this.FilteredExcuses=type2;
+let exDate=this.datePipe.transform(this.NewExcuse.endDate, "dd-MM-yyyy")
+console.log("dd",exDate)
+console.log("dd2",this.NewExcuse.endDate)
+    console.log("this.NewExcuse.startdate", this.NewExcuse.startdate);
+    console.log("this.NewExcuse.endDate", this.NewExcuse.endDate);
+
+    if (this.NewExcuse.professionID != 0) {
+      this.Allexcuses = this.Allexcuses.filter(ex => ex.professionID == this.NewExcuse.professionID)
+      this.FilteredExcuses = this.Allexcuses;
+
     }
+
+    if (this.NewExcuse.employeeId != 0) {
+      this.Allexcuses = this.Allexcuses.filter(ex => (ex.professionID == this.NewExcuse.professionID && ex.employeeId == this.NewExcuse.employeeId)
+        || (ex.date >  this.NewExcuse.startdate && ex.date < this.NewExcuse.endDate))
+      this.FilteredExcuses = this.Allexcuses;
+      console.log("this.Allexcuses", this.Allexcuses);
+
+    }
+
+    // if (this.NewExcuse.startdate != exDate && this.NewExcuse.endDate != exDate) {
+
+    //   let start=this.datePipe.transform(this.NewExcuse.startdate, "yyyy-MM-dd")
+    //   let end=this.datePipe.transform(this.NewExcuse.endDate, "yyyy-MM-dd")
+
+    //   this.Allexcuses = this.Allexcuses.filter(ex => ex.date >  start && ex.date < end)
+    //   this.FilteredExcuses = this.Allexcuses;
+    //   console.log("FilteredExcuses",this.FilteredExcuses)
+    // }
+
+    // var type2 = this.Allexcuses.filter(ex => 
+    //    (ex.professionID == this.NewExcuse.professionID && ex.employeeId == this.NewExcuse.employeeId) 
+    //    ||(ex.date>this.NewExcuse.startdate && ex.date<this.NewExcuse.endDate));
+    //   this.FilteredExcuses=type2;
+  }
 }
